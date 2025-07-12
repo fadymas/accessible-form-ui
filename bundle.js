adb9e55ed40e7b03ce7464e55b43a17646de78a9
fady <fadymahros373@gmail.com>
2025-07-12 04:28:54 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -1,1 +1,49 @@
-console.error("SyntaxError: Can't walk dependency graph: {\n  \"name\": \"accessible-form-ui\",\n  \"version\": \"1.0.0\",\n  \"description\": \"dynamic and accessible form UI built with HTML, CSS and JavaScript. Includes fields for name, email, password, a progress bar, and checklist. Designed with accessibility best practices.\",\n  \"homepage\": \"https://github.com/fadymas/accessible-form-ui#readme\",\n  \"bugs\": {\n    \"url\": \"https://github.com/fadymas/accessible-form-ui/issues\"\n  },\n  \"repository\": {\n    \"type\": \"git\",\n    \"url\": \"git+https://github.com/fadymas/accessible-form-ui.git\"\n  },\n  \"license\": \"ISC\",\n  \"author\": \"\",\n  \"type\": \"commonjs\",\n  \"main\": \"js/master.js\",\n  \"scripts\": {\n    \"sass\": \"sass src/scss/style.scss src/style.css --watch --no-source-map\",\n    \"tailwind\": \"npx @tailwindcss/cli -i ./src/tailwind.css -o ./src/final.css --watch\",\n    \"postcss\": \"npx postcss src/final.css -o dist/output.min.css --watch\",\n    \"browserify\": \" browserify ./src/js/master.js -o ./dist/bundle.js --watch\",\n    \"watchify\": \"watchify src/js/master.js -o dist/bundle.js -v\",\n    \"dev\": \"concurrently  \\\"npm run sass\\\"  \\\"npm run tailwind\\\"  \\\"npm run postcss\\\" \\\"npm run browserify\\\" \\\"npm run watchify\\\" \"و\n      \"build\": \"npm run sass && npm run tailwind && npm run postcss && npm run browserify\",\n\n  },\n  \"devDependencies\": {\n    \"@tailwindcss/postcss\": \"^4.1.11\",\n    \"autoprefixer\": \"^10.4.21\",\n    \"browserify\": \"^17.0.1\",\n    \"concurrently\": \"^9.2.0\",\n    \"cssnano\": \"^7.0.7\",\n    \"gh-pages\": \"^6.3.0\",\n    \"postcss\": \"^8.5.6\",\n    \"postcss-cli\": \"^11.0.1\",\n    \"sass\": \"^1.89.2\",\n    \"watchify\": \"^4.0.0\"\n  }\n}\n : Expected ',' or '}' after property value in JSON at position 1142 (line 23 column 132)\n    required by F:\\projects\\accessible-form-ui\\src\\js\\_fake.js");
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const btn = document.getElementById("btn")
+
+var circle = new ProgressBar.Circle('#container', {
+    color: '#000',
+
+    strokeWidth: 5,
+
+    trailColor: '#eee',
+
+    trailWidth: 3,
+
+    duration: 500,
+
+    easing: 'easeInOut',
+
+
+    text: {
+        value: '0%',
+
+        style: {
+            fontSize: '20px',
+            fontWeight: 'bold',
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: {
+                prefix: true,
+                value: 'translate(-50%, -50%)'
+            },
+            margin: 0,
+            padding: 0
+        },
+
+    },
+
+    step: function (state, circle) {
+
+        var value = Math.round(circle.value() * 100);
+        circle.setText(value + '%');
+    },
+
+});
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    circle.animate(circle.value() + 0.25)
+})
+},{}]},{},[1]);
